5deab2f6122aac22141fc89d41d976a3e2d0d6fb
paridhi.raghuvanshi <paridhi.raghuvanshi009@gmail.com>
2026-07-03 15:41:59 +0530

    Updates

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,7 +5,7 @@ const Footer = () => {
     <footer className="bg-primary py-12">
       <div className="container mx-auto">
         <p className="text-white text-center">
-          Copyright &copy; Nigar Safarova 🤍 2022. All rights reserved.
+          Copyright &copy; Paridhi Raghuvanshi🤍 2026. All rights reserved.
         </p>
       </div>
     </footer>
